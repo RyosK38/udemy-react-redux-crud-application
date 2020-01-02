325ff78f342d2a14dbcdbc95acfe6e64fa5b3268
Ryosuke Kakizaki <kakizaki.r@wingarc.com>
2020-01-02 10:44:44 +0900

    class component and functional

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input
-        type="text"
-        onChange={() => {
-          console.log('I am clicked!')
-        }}
-      />
-    </React.Fragment>
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
   )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App
